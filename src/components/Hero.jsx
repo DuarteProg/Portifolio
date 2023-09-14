8528a1c251e847fa8b5c7760bd64c8229ac03726
DuarteProg <lucas.duartebarbosa@outlook.com>
2023-09-14 17:37:17 -0300

    fixed the gap between the word 'and' and 'web' on Hero section(for mobile)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Lucas</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop user interfaces and
+            I develop user interfaces and &nbsp;
             <br className="xs:block hidden" />
             web applications
           </p>
